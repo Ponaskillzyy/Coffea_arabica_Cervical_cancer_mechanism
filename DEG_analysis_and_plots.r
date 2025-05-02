fc8917d463b5f57c4135383430cdaff8683e2c9b
Ponaskillzyy <111591742+Ponaskillzyy@users.noreply.github.com>
2025-05-02 15:08:49 -0400

    Update DEG_analysis_and_wilcox_plots.r

--- DEG_analysis_and_plots.r
+++ DEG_analysis_and_plots.r
@@ -3,14 +3,14 @@
 ###################
 #Set work directory
 ###################
-setwd("/Users/prosperchukwuemeka/Movies/Ziglar/New_CC_arabica")
+setwd("/Users/prosperchukwuemeka/Movies/Ziglar/New_CC_arabica/Revision")
 
 ###############
 #Load libraries
 ###############
 suppressWarnings(suppressPackageStartupMessages({library(dplyr); library(ggplot2); library(tidyr); 
   library("DESeq2"); library(biomaRt); library(survival);
-  library(stringr); library(edgeR); 
+  library(stringr); library(edgeR); library(ggrepel);
   library(survminer); library(data.table);
   library(tidyverse)}))
 
@@ -18,7 +18,7 @@ suppressWarnings(suppressPackageStartupMessages({library(dplyr); library(ggplot2
 #Download gene expression data of cervical from TCGA
 ####################################################
 #download link
-cervical.exp_mat.link <- "https://gdc-hub.s3.us-east-1.amazonaws.com/download/TCGA-CESC.htseq_counts.tsv.gz"
+cervical.exp_mat.link <- "https://gdc-hub.s3.us-east-1.amazonaws.com/download/TCGA-CESC.star_counts.tsv.gz"
 
 #retrieve gene expression data
 download.file(cervical.exp_mat.link, destfile= paste(getwd(), "cer_dataset.tsv.gz", sep ="/"))
@@ -42,12 +42,17 @@ pseudocnt_count <- function(count){
 #apply function on dataframe to get raw count
 cervical.exp_mat <- apply(cervical.exp_mat, 2, FUN = pseudocnt_count)
 
+#view
 View(cervical.exp_mat)
 
 #cbind Ensembl_ID with cervical.exp.mat
 cervical.exp_mat  <- cbind(Ensembl_ID, cervical.exp_mat) %>% as.data.frame() %>% column_to_rownames(var = "Ensembl_ID")
 
-#View(cervical.exp_mat)
+#view
+View(cervical.exp_mat)
+
+#convert dataframe to matrix
+cervical.exp_mat  <- as.matrix(cervical.exp_mat)
 
 #mapping gene symbol to Ensembl_ID and feature reduction
 #remove version ID from rownames in count matrix
@@ -70,6 +75,9 @@ transcript.info <- transcript.info %>% distinct(hgnc_symbol, .keep_all = TRUE) #
 #annotate "ensembl_ID" with "hgnc_symbol"
 annotation_table <- transcript.info[,1:2]
 
+#convert gene expression data type from matrices to dataframe
+cervical.exp_mat <- as.data.frame(cervical.exp_mat)
+
 #create a column containing ensembl_ID in brca raw_count 
 cervical.exp_mat <- rownames_to_column(cervical.exp_mat)
 
@@ -89,90 +97,95 @@ rownames(cervical.exp_mat) <- cervical.exp_mat$hgnc_symbol
 #delete the column containing "ensembl_gene_id" & "hgnc_symbol"
 cervical.exp_mat <- cervical.exp_mat[, !names(cervical.exp_mat) %in% c("ensembl_gene_id", "hgnc_symbol")]
 
-
 #@@@@@@@@@@@@@@@@@@@@@
 #@@@@@@@@@@@@@@@@@@@@@
 #download link for phenotype data
-phenoData_link <- "https://gdc-hub.s3.us-east-1.amazonaws.com/download/TCGA-CESC.GDC_phenotype.tsv.gz"
+phenoData_link <- "https://gdc-hub.s3.us-east-1.amazonaws.com/download/TCGA-CESC.clinical.tsv.gz"
 
 #retrieve phenodata
 download.file(phenoData_link, destfile= paste(getwd(), "cer_phenoData.tsv.gz", sep ="/"))
 cer_phenodata <- fread(paste0(getwd(), "/cer_phenoData.tsv.gz"))
 
 #subset only samples found in cervical.exp_mat
-cer_phenodata <- cer_phenodata %>% filter(submitter_id.samples %in% colnames(cervical.exp_mat))
+cer_phenodata <- cer_phenodata %>% filter(sample %in% colnames(cervical.exp_mat))
 
-#subset "Normal" $ "primary Tumor"
+#########
+#metadata to look into "disease_type (primary tumor, no metastatic disease)", "age_at_index.demographic (20 to 88 years)", "treatment_or_therapy.treatments.diagnoses (therapy naive)"
+#########
+#subset "primary Tumor"
 cer_phenodata <- cer_phenodata %>% 
-  filter(sample_type.samples %in% c("Primary Tumor", "Solid Tissue Normal")) %>% as.data.frame() %>% 
-  column_to_rownames(var = "submitter_id.samples")
+  filter(sample_type.samples %in% c("Primary Tumor")) %>% as.data.frame() %>% 
+  column_to_rownames(var = "sample")
 
-#subset only samples that are "Metastatic" & "Primary Tumor" in cervical.exp_mat
+#subset only samples that are "Primary Tumor" in cervical.exp_mat
 cervical.exp_mat <- cervical.exp_mat[, colnames(cervical.exp_mat) %in% rownames(cer_phenodata)] %>% as.data.frame()
 
-#check if desired sample type is appropriately retrieved
-#table(cer_phenodata$sample_type.samples)
 
-#View phenodata 
-head(cer_phenodata)
+###########
+#Load GTEx normal cervix tissue expression data
+###########
+GTEx.normal.tissue.exp_mat.ecto <- fread(paste0("/Users/prosperchukwuemeka/Movies/Ziglar/New_CC_arabica/Cervical-cancer-healthy-tissue-ExpressionData", "/gene_reads_cervix_ectocervix.gct")) %>% dplyr::select(-Name) 
+GTEx.normal.tissue.exp_mat.endo <- fread(paste0("/Users/prosperchukwuemeka/Movies/Ziglar/New_CC_arabica/Cervical-cancer-healthy-tissue-ExpressionData", "/gene_reads_cervix_endocervix.gct")) %>% dplyr::select(-Name) 
 
-#change level
-cer_phenodata$sample_type.samples <- ifelse(cer_phenodata$sample_type.samples == "Solid Tissue Normal",
-                                            "Normal tissue", "Primary Tumor")
+#check if description in both dataset matches
+all(GTEx.normal.tissue.exp_mat.ecto$Description %in% GTEx.normal.tissue.exp_mat.endo$Description)
 
-#@@@@@@@@@@@@@@@@@@@@@
-#@@@@@@@@@@@@@@@@@@@@@
-#subset samples in "cervical.exp_mat" that is contained in "cer_phenodata" (i.e samples with phenotype data)
-cervical.exp_mat <- cervical.exp_mat[, colnames(cervical.exp_mat) %in% rownames(cer_phenodata)] %>% as.data.frame()
+#cbind both GTEx dataset
+GTEx.cervix.normal.tissue.exp_mat <- cbind(GTEx.normal.tissue.exp_mat.ecto, GTEx.normal.tissue.exp_mat.endo) %>% as.data.frame() 
 
-#check dimension of "cervical.exp_mat" & "cer_phenodata"; note that n_samples must be the same in all three dataframe
-dim(cervical.exp_mat)
-dim(cer_phenodata)
+#remove duplicate columns
+names(GTEx.cervix.normal.tissue.exp_mat)[1] <- "Gene"
+GTEx.cervix.normal.tissue.exp_mat <- GTEx.cervix.normal.tissue.exp_mat %>% dplyr::select(-Description)
 
-#retrieve transcript_length from biomart
-transcript <- data.frame(transcript.info[, c("hgnc_symbol", "transcript_length")])
+#select only genes found in cervical cancer expression Data
+GTEx.cervix.normal.tissue.exp_mat <- GTEx.cervix.normal.tissue.exp_mat %>% .[.$Gene %in% rownames(cervical.exp_mat),]
 
-#make a dataframe of genes present in cervical.exp_mat
-hgnc_symbol <- as.data.frame(rownames(cervical.exp_mat), row.names = rownames(cervical.exp_mat))
-colnames(hgnc_symbol) <- "hgnc_symbol"
+#check for duplicate rows based on gene column
+any(duplicated(GTEx.cervix.normal.tissue.exp_mat$Gene))
 
-#join hgnc_symbol dataframe $ transcript dataframe to get 
-transcript <- left_join(hgnc_symbol, transcript, 
-                        by='hgnc_symbol') #%>% column_to_rownames(var = "hgnc_symbol")
+#remove duplicates while retaining first occurrence
+GTEx.cervix.normal.tissue.exp_mat <- GTEx.cervix.normal.tissue.exp_mat %>% .[!duplicated(.$Gene), ] 
 
-rownames(transcript) <- transcript$hgnc_symbol
+#rename rownames with gene symbol
+rownames(GTEx.cervix.normal.tissue.exp_mat) <- GTEx.cervix.normal.tissue.exp_mat$Gene
+GTEx.cervix.normal.tissue.exp_mat <- GTEx.cervix.normal.tissue.exp_mat %>% dplyr::select(-Gene) %>% as.data.frame()
 
-#View transcript
-#head(transcript)
+#filter out genes not present in healthy cervix tissue from cervical cancer expression data 
+cervical.exp_mat <- cervical.exp_mat %>% .[rownames(.) %in% rownames(GTEx.cervix.normal.tissue.exp_mat),]
 
-#@@@@@@@@@@@@@@@@@@@@@
-#@@@@@@@@@@@@@@@@@@@@@
-#Assume cervical.exp_mat and cer_phenodata are your data objects
-# Get the column names of cervical.exp_mat
-exp_mat_names <- colnames(cervical.exp_mat)
+#reorder the gene names of the healthy tissue expression data by the cervical cancer gene order
+GTEx.cervix.normal.tissue.exp_mat <- GTEx.cervix.normal.tissue.exp_mat[match(rownames(cervical.exp_mat), rownames(GTEx.cervix.normal.tissue.exp_mat)), ]
 
-#Reorder the rows of cer_phenodata to match the column names of cervical.exp_mat
-cer_phenodata_ordered <- cer_phenodata[match(exp_mat_names, rownames(cer_phenodata)), ]
+###########
+#make phenotype data for GTEx normal cervix tissue expression data
+###########
+GTEx.cervix.phenoData <- data.frame("sample" = colnames(GTEx.cervix.normal.tissue.exp_mat), row.names = colnames(GTEx.cervix.normal.tissue.exp_mat))
+GTEx.cervix.phenoData$sample_type.samples <- "Normal Cervix Tissue"
 
-# Check if the reordering was successful
-head(rownames(cer_phenodata_ordered))
+###########
+#make new phenotype data concatenating TCGA tumor data and GTEx normal tissue data
+###########
+phenoData <-  bind_rows(cer_phenodata, GTEx.cervix.phenoData)
+
+###########
+#make new Expression data concatenating TCGA tumor data and GTEx normal tissue data
+###########
+ExpressionData <- bind_cols(cervical.exp_mat, GTEx.cervix.normal.tissue.exp_mat)
+
+#check dimension of "ExpressionData" & "phenoData"; note that n_samples must be the same in all dataframe
+dim(ExpressionData)
+dim(phenoData)
+
+#reorder samples in phenoData to match ExpressionData
+phenoData <- phenoData %>% .[match(colnames(ExpressionData), rownames(.)), ]
 
 ##############################
 #Normalization by TMM in edgeR
 ##############################
 #Organize data in a summarized experiment (se) for downstream
-cer_se <- SummarizedExperiment(assays = cervical.exp_mat,
-                               rowData = rownames(transcript),
-                               colData = cer_phenodata_ordered)
-
-# #save summarized experiment as RDS for posterity
-# saveRDS(cer_se, "/Users/prosperchukwuemeka/Movies/Ziglar/cer_se.rds")
-# 
-# #read summarized experiment
-# cer_se <- readRDS("cer_se.rds")
-# #assay(cer_se)
-# #colData(cer_se)
-# #rowData(cer_se)
+cer_se <- SummarizedExperiment(assays = ExpressionData,
+                               rowData = rownames(ExpressionData),
+                               colData = phenoData)
 
 #normalization by trimmed mean of M (TMM)
 #create factor for all samples
@@ -239,12 +252,23 @@ res$Gene <- rownames(res)
 res$Expression = ifelse(res$FDR < 0.05 & abs(res$logFC) >= 1, 
                         ifelse(res$logFC>= 1 ,'Up-regulated in Tumor','Down-regulated in Tumor'),
                         'Stable')
+#order gene expression
+res <- res %>% 
+  group_by(Expression) %>%
+  arrange(desc(logFC)) %>% column_to_rownames("Gene")
+res$Gene <- rownames(res)
 
 # #save DEG result
 # saveRDS(res, "cc_deg_result.rds")
-# 
+
 #readRDS
 res <- readRDS("cc_deg_result.rds")
+
+#filter upregulated genes
+upreg.gene <- res %>% filter(Expression == "Up-regulated in Tumor")
+
+#write csv
+write_csv(upreg.gene, "upreg.gene.csv")
 
 #@@@@@@@@@@@@@@@@@@@@@
 #@@@@@@@@@@@@@@@@@@@@@
@@ -281,12 +305,12 @@ volcano.theme <- theme(strip.text = element_text(face = "bold", size = 20),
 #remove genes with NA as adjpvalue
 res <- na.omit(res)
 
-#Apply a small offset to p-value
-offset <- 0 + 1e-50 #prevent having a pvalue of exactly zero since log10(0) is undefined
-
-#index genes with padj with exactly 0
-ndx.0.padj <- which(res$FDR==0)
-res[ndx.0.padj,] <- offset
+# #Apply a small offset to p-value
+# offset <- 0 + 1e-50 #prevent having a pvalue of exactly zero since log10(0) is undefined
+# 
+# #index genes with padj with exactly 0
+# ndx.0.padj <- which(res$FDR==0)
+# res[ndx.0.padj,] <- offset
 
 #Plot with ggplot
 res.plot <- ggplot(data = res, 
@@ -331,99 +355,35 @@ res <- res.plot +
 # Display the plot
 print(res) 
 
-
-########
-#PLOT 2
-########
-#readRDS
-res <- readRDS("cc_deg_result.rds")
-
-#select core compound targeted genes in cervical cancer
-compound_disease_signature <- c("PLAU", "TPX2", "TPI1", "FABP5", "KIF11", "GPI", "ESRRA", "AURKA", "MTHFD1", 
-                                "UMPS", "GM2A", "RANBP1", "TYMS", "GART", "CCNA2", "STAT1", "DOT1L", "MMP12", 
-                                "PARP1", "VDR", "HMGCR", "TK1", "MDM2", "SYK", "DTYMK", "HPRT1", "ADK", 
-                                "CA2", "CDK2", "ACP3", "SEC14L2", "GALE", "YARS1", "SULT2B1", "TYMP", 
-                                "S100A9", "UCK2", "MMP9")
-
-#remove genes with NA as adjpvalue
-res <- na.omit(res)
-
-#Apply a small offset to p-value
-offset <- 0 + 1e-50 #prevent having a pvalue of exactly zero since log10(0) is undefined
-
-#index genes with padj with exactly 0
-ndx.0.padj <- which(res$FDR==0)
-res[ndx.0.padj,] <- offset
-
-#Plot with ggplot
-res.plot <- ggplot(data = res, 
-                   aes(x = logFC, 
-                       y = -log(FDR, 10), 
-                       colour= Expression)) + 
-  geom_point(size=3.5) +
-  xlim(c(-6, 6)) +
-  geom_vline(xintercept=c(-1,1),lty=4,col="black",lwd=0.8) +
-  geom_hline(yintercept = -log(0.05, 10),lty=4,col="black",lwd=0.8) +
-  labs(x=expression(bold("Log"[2]*"(Fold Change)")),
-       y= expression(bold("-Log"[10]*"(FDR)")),
-       title="Overlapping gene targets between cervical cancer DEGs and C. arabica compound-derived targets")  + 
-  theme_classic() + volcano.theme + theme(legend.position = "top")
-
-#view
-print(res.plot)
-
-#add labels and connectors for top enriched genes
-compound_disease_signature_length <- 38
-
-#extract the data of compound targeted genes in cervical cancer
-compound_disease_signature.dat <- res %>% 
-  filter(res$Gene %in% compound_disease_signature)
-
-#show gene of interest
-res <- res.plot + 
-  geom_label_repel(data = compound_disease_signature.dat,
-                   mapping = aes(logFC, -log(FDR,10), label = Gene, fontface = "bold"), direction = "both",
-                   size = 3, nudge_x = 0.1, box.padding = 0.5,  show.legend = FALSE, max.overlaps = Inf,
-                   nudge_y = 0.2) + guides(color = guide_legend(override.aes = list(size = 5))) 
-
-# Display the plot
-print(res) 
-
 #@@@@@@@@@@@@@@@@@
 #@@@@@@@@@@@@@@@@@
+#load compound-disease shared targets
+compound_disease_targets <- fread("Common Targets.csv") %>% pull(`Common Targets`)
+
 #ndx for stratifying patients based on tissue biopsy
-normal_tissue_idents <- rownames(dge$samples)[which(dge$samples$sample_type.samples == "Normal tissue")]
-primary_tumor_idents <- rownames(dge$samples)[which(dge$samples$sample_type.samples == "Primary Tumor")]
+normal_tissue_idents <- rownames(dge$samples)[which(dge$samples$group == "Normal Cervix Tissue")]
+primary_tumor_idents <- rownames(dge$samples)[which(dge$samples$group == "Primary Tumor")]
 
 #get expression matrix
 expression_data <- cervicalData_tmm_normalize
 
-#select "CCNA2" for statistical analysis
-cervical_cancer_biomarker <- c("CCNA2")
-
 #subset expression data for gene of interest
-expression_data <- as.data.frame(expression_data[rownames(cervicalData_tmm_normalize) %in% cervical_cancer_biomarker,])
-names(expression_data) <- "CCNA2"
+expression_data <- as.data.frame(expression_data[rownames(cervicalData_tmm_normalize) %in% compound_disease_targets,]) %>% t()
 
 #get tissue details from phenotype data
-phenotype_data <- dge$samples %>% dplyr::select(sample_type.samples)
+phenotype_data <- dge$samples %>% dplyr::select(group)
 names(phenotype_data) <- "Tissue"
 
-#match patient ident in expression and pData
-phenotype_data <- phenotype_data[match(rownames(expression_data), rownames(phenotype_data)), ]
+#Combine the dataframes by column
+expression_pData <- bind_cols(expression_data, phenotype_data)
 
-#Combine the data frames by column
-expression_pData <- cbind(expression_data, phenotype_data)
-
-#rename column
-names(expression_pData)[2] <- "Tissue"
+#convert data to long format
+expression_pData <- expression_pData %>% rownames_to_column(var = "Sample") %>%
+  pivot_longer(names_to = "Gene", values_to = "Expression", 2:47)
 
 #####
 #plot graph with statistical significance
 #####
-#Define your specific comparisons
-comparisons_list <- list(c("Normal tissue", "Primary Tumor"))
-
 #themes
 themes <- theme(strip.text = element_text(face = "bold", size = 15), panel.border = element_rect(color = "black", fill = NA, size = 1),
                 legend.text = element_text(face = "bold", size = 15),legend.title = element_text(face = "bold", size = 15),
@@ -433,17 +393,33 @@ themes <- theme(strip.text = element_text(face = "bold", size = 15), panel.borde
                 axis.ticks.x = element_blank(), legend.position = "None",
                 panel.background = element_rect(fill = "white"), axis.line = element_line(linewidth = 0.8, colour = "black"))
 
-expression_pData %>%
-  ggplot(., aes(x=Tissue, y= CCNA2)) +
-  geom_boxplot(aes(fill = Tissue), outlier.color = NA) + theme_classic() +
-  #geom_jitter(size = 0.2, alpha = 0.5) +
-  stat_compare_means(comparisons = comparisons_list, method = "wilcox.test", 
-                     label = "p.signif", p.adjust.method = "BH", 
-                     vjust = 0, 
-                     map_signif_level = TRUE, step_increase = 0.1, 
-                     textsize = 6, size = 7, 
-                     tip_length = 5, na.rm = T) +
-  labs(y= "Expression log(cpm)", x = " ", fill = "Tissue") +
-  theme_classic() +
+p <- expression_pData %>%
+  ggplot(., aes(x=Tissue, y= Expression)) +
+  geom_boxplot(outlier.shape = NA, fill = "grey80", show.legend = FALSE) +
+  geom_point(aes(color = Tissue), size = 4, alpha = 1, position = position_jitter(width = 0.03, height = 0)) +
+  labs(title = "Expression of Shared Compound-Cervical Cancer Targets in Normal (n=47) vs. Primary Tumor (n=304)", 
+       x = "Gene", 
+       y = "Expression (cpm)", 
+       color = "Tissue") + 
+  theme_bw() + 
   themes +
-  ggtitle("CCNA2 expression in primary tumor vs normal tissue")
+  theme(legend.position = "right") + # Keep the legend at the top
+  facet_wrap(~Gene, scales = "free", nrow = 7) 
+
+#Add statistical analysis
+p + stat_compare_means(method = "wilcox.test", aes(group = Tissue), 
+                       fontface = "bold", label = "p.signif", hide.ns = T, 
+                       size = 8, vjust = 0.7)
+
+
+
+
+
+
+
+
+
+
+
+
+
